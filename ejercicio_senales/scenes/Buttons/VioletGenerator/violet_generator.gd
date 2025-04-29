74eccce60ac91832bda87a488632427eab5b070d
giulibaez <giulibaez56@gmail.com>
2025-04-29 19:58:30 -0300

    Creacion de func generate_block

--- ejercicio_senales/scenes/Buttons/VioletGenerator/violet_generator.gd
+++ ejercicio_senales/scenes/Buttons/VioletGenerator/violet_generator.gd
@@ -2,4 +2,10 @@ extends TextureButton
 
 
 func _on_button_down() -> void:
-	pass # Replace with function body.
+	generate_block()
+	
+
+func generate_block ():
+	var block_scene = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
+	var instance = block_scene.instantiate()
+	add_child(instance)
