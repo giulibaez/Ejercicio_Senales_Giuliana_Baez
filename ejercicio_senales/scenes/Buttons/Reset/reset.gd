caeda16f4ebcbf8a4332633ac40eac14b9c25061
giulibaez <giulibaez56@gmail.com>
2025-04-29 21:31:10 -0300

    función delete del menu

--- ejercicio_senales/scenes/Buttons/Reset/reset.gd
+++ ejercicio_senales/scenes/Buttons/Reset/reset.gd
@@ -5,4 +5,5 @@ func _on_button_down() -> void:
 	var blocks= get_tree().get_nodes_in_group("blocks")
 	for block in blocks:
 		block.delete()
-	
+	var menu = get_parent()
+	menu.delete()  
--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -10,10 +10,13 @@ func _ready():
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 	$RedGenerator.button_down.connect(self.count_new_instance)
 	$VioletGenerator.button_down.connect(self.count_new_instance)
-
 	
 
 func count_new_instance():
 	instancesCount +=1
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+
+func delete():
+	instancesCount = 0
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 	
